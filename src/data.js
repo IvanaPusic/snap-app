5f27fe10e1ca38573190aa7234dbf99d69319191
Ivana Pusic <pusicka97@gmail.com>
2024-05-29 15:25:33 +0200

    create links for navigation

---
+++ src/data.js
@@ -0,0 +1,59 @@
+import bell from './assets/bell.svg';
+import calendar from './assets/calendar.svg';
+import planning from './assets/planning.svg';
+import todo from './assets/todo.svg';
+
+export const links = [
+  {
+    id: 1,
+    link: 'Features',
+    subLinks: [
+      {
+        id:1,
+        img: todo,
+        link: 'Todo List'
+      },
+      {
+        id:2,
+        img: calendar,
+        link: 'Calendar'
+      },
+      {
+        id:3,
+        img: bell,
+        link: 'Reminder'
+      },
+      {
+        id:4,
+        img: planning,
+        link: 'Planning'
+      },
+    ]
+  },
+  {
+    id:2, 
+    link: 'Company',
+    sublinks:[
+      {
+        id:1,
+        link: 'History',
+      },
+      {
+        id:2,
+        link: 'Our Team',
+      },
+      {
+        id:3,
+        link: 'Blog',
+      },
+    ],
+  },
+  {
+    id: 3,
+    link: 'Careers',
+  },
+  {
+    id: 4,
+    link: 'About',
+  },
+]
