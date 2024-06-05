b1c67df8dcaef0c70f2d9794885c9711af31f4da
Ivana Pusic <pusicka97@gmail.com>
2024-06-05 15:02:36 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,72 @@ import menu from './assets/menu.svg';
 import partners from './assets/partners.svg';
 import CompanyModal from './components/CompanyModal';
 import FeaturesModal from './components/FeaturesModal';
+import NavigationModal from './components/NavigationModal';
 import { links } from './data';
 
 function App() {
   const [navLinks, setNavLinks] = useState(links);
   const [isCompanyModalOpen, setIsCompanyModalOpen] = useState(false);
   const [isFeaturesModalOpen, setIsFeaturesModalOpen] = useState(false);
+  const [isCompanyMobileModalOpen, setIsCompanyMobileModalOpen] = useState(false);
+  const [isFeaturesMobileModalOpen, setIsFeaturesMobileModalOpen] = useState(false);
   const [companySublinks, setCompanySublinks] = useState(links.filter(item => item.link === 'Company'));
   const [featuresSublinks, setFeaturesSublinks] = useState(links.filter(item => item.link === 'Features'));
+  const [isNavModalOpen, setIsNavModalOpen] = useState(false);
 
   const handleClick = item => {
-    if(item.link === 'Features') {
+    if (item.link === 'Features') {
       setIsFeaturesModalOpen(!isFeaturesModalOpen);
+      if (window.innerWidth <= 1024) {
+        setIsFeaturesMobileModalOpen(!isFeaturesMobileModalOpen);
+      }
     }
-    if(item.link === 'Company') {
+    if (item.link === 'Company') {
       setIsCompanyModalOpen(!isCompanyModalOpen);
+      if (window.innerWidth <= 1024) {
+        setIsCompanyMobileModalOpen(!isCompanyMobileModalOpen);
+      }
     }
-  }
+  };
+
+  const openNavModal = () => {
+    console.log('open modal');
+    setIsNavModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsNavModalOpen(false);
+  };
+
   return (
     <div className='lg:relative'>
       {/* Header */}
-      {isCompanyModalOpen && <CompanyModal companySublinks={companySublinks}/> }
-      {isFeaturesModalOpen && <FeaturesModal featuresSublinks={featuresSublinks}/> }
+      {isNavModalOpen && (
+        <NavigationModal
+          closeModal={closeModal}
+          navLinks={navLinks}
+          handleClick={handleClick}
+          companySublinks={companySublinks}
+          featuresSublinks={featuresSublinks}
+          isCompanyModalOpen={isCompanyModalOpen}
+          isFeaturesModalOpen={isFeaturesModalOpen}
+        />
+      )}
+      {isCompanyModalOpen && <CompanyModal companySublinks={companySublinks} />}
+      {isFeaturesModalOpen && <FeaturesModal featuresSublinks={featuresSublinks} />}
       <header>
         <nav className='navigation'>
           <div className='navigation-wrapper'>
             <div className='logo-wrapper'>
               <img src={logo} alt="logo" />
             </div>
-            <button className='menu-btn'>
+            <button className='menu-btn' onClick={openNavModal}>
               <img src={menu} alt="menu" />
             </button>
             <ul className='links-wrapper'>
               {navLinks.map(item => (
-                <li key={item.id} className='link' >
-                  <button href={item.link} onClick = {() => handleClick(item)}>
+                <li key={item.id} className='link'>
+                  <button href={item.link} onClick={() => handleClick(item)}>
                     <span>{item.link}</span>
                     {item.img && <img src={item.img} alt={item.link} className='inline-block ml-1' />}
                   </button>
@@ -48,7 +79,7 @@ function App() {
             </ul>
           </div>
           <div className='login-wrapper'>
-            <button>Login</button>
+            <button className='login-btn'>Login</button>
             <button className='register-btn'>Register</button>
           </div>
         </nav>
