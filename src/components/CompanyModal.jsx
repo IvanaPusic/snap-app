8ff38a1baaccdc4a0e930af8f22c146d78d5acc7
Ivana Pusic <pusicka97@gmail.com>
2024-06-05 15:01:40 +0200

    refactor code

--- src/components/CompanyModal.jsx
+++ src/components/CompanyModal.jsx
@@ -3,20 +3,24 @@ import { useEffect } from "react";
 const CompanyModal = ({ companySublinks }) => {
 
   useEffect(() => {
-    console.log(companySublinks[0].subLinks)
-  },[]);
-   
+    console.log(companySublinks[0].subLinks);
+  }, []);
+
   return (
-    <div className="company">
-      {
-        companySublinks[0].subLinks.map(item => {
-          return <li key ={item.id} className="company-list-item">
-            <a className= 'company-link' href={item.link}>{item.link}</a>
-          </li>
-        })
-      }
-    </div>
-  )
+    <>
+      <div className="company">
+        {
+          companySublinks[0].subLinks.map(item => {
+            return (
+              <li key={item.id} className="company-list-item">
+                <a className="company-link" href={item.link}>{item.link}</a>
+              </li>
+            );
+          })
+        }
+      </div>
+    </>
+  );
 }
 
-export default CompanyModal
+export default CompanyModal;
